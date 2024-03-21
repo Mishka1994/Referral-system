cc640c78f9f7ecba9a2a0e3076e9d1946e7acef5
Mihail Samofalov <misasamofalov611@gmail.com>
2024-03-21 22:08:03 +0700

    edit view_for_api file and description of the project in README

--- users/service.py
+++ users/service.py
@@ -3,7 +3,17 @@ import string
 import time
 import random
 
+from users.models import User
+
+
+def get_referral_users(code):
+    """ Функция для формирования списка поль-ей, которые использовали инвайт-код данного пользователя"""
+    users = User.objects.filter(someone_invite_code=code)
+    list_users = [str(user) for user in users]
+    return list_users
+
 def generate_authorization_code():
+    """ Функция для формирования кода авторизации """
     time.sleep(3)
     authorization_code = str()
     for _ in range(4):
@@ -12,6 +22,7 @@ def generate_authorization_code():
 
 
 def generate_invite_code():
+    """ Функция для формирования личного инвайт-кода """
     letters_and_digits = string.ascii_letters + string.digits
     invite_code = ''.join(secrets.choice(
         letters_and_digits) for i in range(6))
