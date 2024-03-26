52b4bc2c39b80b97d140bdd3304ec62de15acbc1
Mihail Samofalov <misasamofalov611@gmail.com>
2024-03-26 15:30:43 +0700

    debug curl`s request in README

--- users/permissions.py
+++ users/permissions.py
@@ -8,7 +8,9 @@ class IsAuthorization(BasePermission):
     def has_permission(self, request, view):
         input_phone = request.data.get('phone')
         if input_phone is None:
-            raise AttributeError('Введите телефон пользователя!')
+            input_phone = request.query_params.get('phone')
+            if input_phone is None:
+                return False
         user = User.objects.filter(phone=input_phone).first()
         if user is None:
             return False
